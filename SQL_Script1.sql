9a082679be7f086c5a1884015a866c006d2dd099
TheLastPossibleUsernameEver <39888077+TheLastPossibleUsernameEver@users.noreply.github.com>
2019-07-04 14:33:41 +0000

    Update SQL_Script1.sql

--- SQL_Script1.sql
+++ SQL_Script1.sql
@@ -56,7 +56,6 @@ CREATE TABLE IF NOT EXISTS `tfdb`.`case` (
   `update_stamp` TIMESTAMP NULL,
   PRIMARY KEY (`case_id`, `project_id`),
   INDEX `fk_case_project1_idx` (`project_id` ASC),
-  CONSTRAINT `fk_case_project1`
     FOREIGN KEY (`project_id`)
     REFERENCES `tfdb`.`project` (`project_id`)
     ON DELETE NO ACTION
@@ -73,12 +72,10 @@ CREATE TABLE IF NOT EXISTS `tfdb`.`project_user_mapping` (
   PRIMARY KEY (`project_project_id`, `user_user_id`),
   INDEX `fk_project_has_user_user1_idx` (`user_user_id` ASC),
   INDEX `fk_project_has_user_project_idx` (`project_project_id` ASC),
-  CONSTRAINT `fk_project_has_user_project`
     FOREIGN KEY (`project_project_id`)
     REFERENCES `tfdb`.`project` (`project_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_project_has_user_user1`
     FOREIGN KEY (`user_user_id`)
     REFERENCES `tfdb`.`user` (`user_id`)
     ON DELETE NO ACTION
