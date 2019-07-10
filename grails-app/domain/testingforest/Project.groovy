67c529f8cfbc65fd201eb74ebf9bdf0418ffb4f8
DmitryPogrebnoy <pogrebnoy.inc@gmail.com>
2019-07-10 10:11:36 +0300

    Add domain classes with associations Add delete TestCase

---
+++ grails-app/domain/testingforest/Project.groovy
@@ -0,0 +1,15 @@
+package testingforest
+
+class Project {
+
+    String projectName
+    Date dateCreated
+    Date lastUpdated
+
+    static hasMany = [teamList : User , testCaseList : TestCase]
+
+    static constraints = {
+        projectName size: 1..255
+        testCaseList nullable: true
+    }
+}
---
+++ grails-app/domain/testingforest/User.groovy
@@ -0,0 +1,23 @@
+package testingforest
+
+class User {
+
+    String login
+    String password
+    String userName
+    String role
+    Date dateCreated
+    Date lastUpdated
+
+    //User created any TestCase
+    static hasMany = [caseList : TestCase]
+
+    static constraints = {
+        login size: 5..30, matches: "[a-zA-Z0-9]+", unique: true
+        password size: 5..30, password: true
+        userName size: 1..255, matches: "[a-zA-Z0-9]+"
+        role inList: ["admin", "user"]
+        project nullable: true
+        caseList nullable: true
+    }
+}
