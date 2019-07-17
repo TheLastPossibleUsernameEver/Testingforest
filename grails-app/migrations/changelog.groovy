8af21caf0fa20b7d8f6cf499fae4b202ef5e087d
shandakova <shandakova98@mail.ru>
2019-07-17 22:17:21 +0300

    Fixed updating

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,3 +1,4 @@
 databaseChangeLog = {
     include file:'create-table.groovy'
+    include file: 'update-tables-1.groovy'
 }
---
+++ grails-app/migrations/update-tables-1.groovy
@@ -0,0 +1,8 @@
+databaseChangeLog = {
+
+    changeSet(author: "galina (generated)", id: "1563390655247-1") {
+        dropUniqueConstraint(constraintName: "UC_USERLOGIN_COL", tableName: "user")
+
+        addUniqueConstraint(columnNames: "login", constraintName: "UC_USERLOGIN_COL", tableName: "user")
+    }
+}
