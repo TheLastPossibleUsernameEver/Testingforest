5e8c25cb51f728686971f5c4dffc083bc599c9f8
root <RuS.Ghost@yandex.ru>
2019-08-09 08:43:22 +0000

    Added blank test for DocumentFormat

---
+++ src/test/groovy/testingforest/TestCaseDocumentFormatSpec.groovy
@@ -0,0 +1,18 @@
+package testingforest
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class TestCaseDocumentFormatSpec extends Specification implements DomainUnitTest<TestCaseDocumentFormat> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
